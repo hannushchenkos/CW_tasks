dbb4964aec815929000bc422161b2d24708a1ec9
o.hannushchenko <hannushchenkos@gmail.com>
2023-05-21 00:06:54 +0300

    update(fundamentals:added app16.js):fix bug

--- script/fundamental/app16.js
+++ script/fundamental/app16.js
@@ -5,10 +5,12 @@
 // If the input array is empty consider it as: [0] (array with a zero).
 
 function oddOrEven(array) {
-   let a = array.reduce((acc, item)=>{
-     return acc + item;
-   }, 0)
-   return a%2 === 1 ? "odd" : "even";
+  let a = array.reduce((acc, item)=>{
+      console.log(acc);
+    return acc + item;
+  }, 0)
+  console.log(a);
+  return Math.abs(a%2) === 1 ? "odd" : "even";
 }
 
-console.log(oddOrEven([-3, 1, 15]));
+console.log(oddOrEven([-796347,-79979,145275,-753901,-973109,-258863,133129,829705,167865,689791,-77507]));
