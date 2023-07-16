85e2fadc7e0f3d7030d8052f640a6361e00b26e8
o.hannushchenko <hannushchenkos@gmail.com>
2023-07-17 00:01:19 +0300

    update(fundamentals:added app81.js):Count football points(update)

--- script/fundamental/app81.js
+++ script/fundamental/app81.js
@@ -28,4 +28,8 @@ function points(games) {
     return finalPoints;
 }
 
+// const points=games=>games.reduce((output,current)=>{
+//     return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+// },0)
+
 console.log(points(['4:1','4:1','4:1','4:1','4:1','4:1','4:1']));
